f0bde9c8b40505df884d6389d7e6c426bf771bb6
Abhisakh Sarma <abhisakh@gmail.com>
2026-03-22 09:15:57 +0100

    feat(auth.sql): add role column with enforced RBAC constraints
    - Added  column to abhisakhsarma table (employee/hr/admin)
    - Added CHECK constraint to restrict values
    - Ensures each user has a defined role for role-based access control
    - Default role set to 'employee' for new users

--- backend/app/db/schemas/auth.sql
+++ backend/app/db/schemas/auth.sql
@@ -1,9 +1,19 @@
 -- Schema for User Authentication
+-- CREATE TABLE IF NOT EXISTS users (
+--     user_id TEXT PRIMARY KEY,
+--     username TEXT UNIQUE NOT NULL,
+--     password_hash TEXT NOT NULL,
+--     role TEXT DEFAULT 'employee',
+--     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (user_id) REFERENCES employees(user_id)
+-- );
+
+
 CREATE TABLE IF NOT EXISTS users (
     user_id TEXT PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    role TEXT DEFAULT 'employee',
+    role TEXT NOT NULL DEFAULT 'employee' CHECK(role IN ('employee', 'hr', 'admin')),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES employees(user_id)
 );
